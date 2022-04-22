47f24d0e7554f81c4ca6e414987f792f66f4cbbd
Tonix22 <emiliotonix@gmail.com>
2022-04-21 21:33:08 -0500

    Matrix add some comments

--- PythonScripts/matrix_fixed_gen.py
+++ PythonScripts/matrix_fixed_gen.py
@@ -15,23 +15,30 @@ np.set_printoptions(suppress=True,
 pd.set_option('display.float_format', lambda x: '%.40f' % x)
 
 for n in range (0,REALIZATIONS):
+    #Flatten matrix 2D in a 1D array
     rand_arry = np.random.uniform(-1.0,1,MATRIX_SIZE)
+    #reshaping used to get optimial multiplication
     matrix    = np.reshape(rand_arry,(ROW_SIZE,ROW_SIZE))
+    #Vector Generation
     vector    = np.random.uniform(-1.0,1,ROW_SIZE)
+    #optimal value, 
+    #FXP will aproach to this value using only 16 bits
     f_result = matrix@vector.T
 
+    #FXP generation truncation is done by the internal 
+    #library with the QIF required.
     z1 = Fxp(rand_arry, signed=True, dtype='Q2.14')
     z2 = Fxp(vector, signed=True, dtype='Q2.14')
+    
+    #Save data in pandas data frame
     df.loc[n*2,0:63]    = rand_arry
     df.loc[n*2,64:71]   = vector
     df.loc[n*2,72:79]   = f_result
 
-    
     for m in range(0,64):
         df.loc[n*2+1,m]  = z1[m].hex() 
     for m in range(0,8):
         df.loc[n*2+1,m+64]  = z2[m].hex() 
-
 
 df.to_csv('data.csv')
 #print(z1.bin(frac_dot=True))
